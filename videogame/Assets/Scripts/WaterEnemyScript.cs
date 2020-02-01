223b6f24f24019fd55f999f61c393bbfb7e325b5
Christian Sumares <christiansumares@gmail.com>
2020-02-01 14:27:01 -0800

    Water Enemy Movement

---
+++ videogame/Assets/Scripts/WaterEnemyScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterEnemyScript : MonoBehaviour
+{
+
+    public Vector2 moveDirection;
+    public float speed;
+       
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+
+    void FixedUpdate() {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        if (rb.velocity.magnitude < 10) {
+            //Debug.Log("Faster!");
+            rb.AddRelativeForce(moveDirection * speed - rb.velocity);
+        }
+        else {
+            //Debug.Log("Too fast!");
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        moveDirection *= -1;
+    }
+}
