6977a8b0e69b6565b714fa3f044ea5269da82454
Erik B Gonzalez <50939207+alta010@users.noreply.github.com>
2020-02-02 14:26:59 -0800

    update

---
+++ videogame/Assets/Erik's F/EScripts/HealthBar.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    public ProgressBar pb;
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
