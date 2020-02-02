f7c6daef7b7083e1c657312cf8b8a16a420d209f
Erik B Gonzalez <50939207+alta010@users.noreply.github.com>
2020-02-02 12:05:43 -0800

    HealthBar

---
+++ videogame/Assets/FillStatusBar.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FillStatusBar : MonoBehaviour
+{
+    public PlayerHealth playerhealth;
+    public Image fillImage;
+    private Sldier slider;
+
+    void Awake()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    void Update()
+    {
+        if (slider.value <= slider.minValue)
+        {
+            fillImage.enable = false;
+        }
+
+        if (slider.value > slider.minvalue && !fillImage.enabled)
+        {
+            fillImage.enable = true;
+        }
+
+        float fillValue = playerHealth.currentHealth / playerhealth.maxHealth;
+        if (fillValue <= slider.maxvalue)
+        {
+            fillImage.color = Color.white;
+        }
+
+        slider.value = fillValue;
+    }
+}
